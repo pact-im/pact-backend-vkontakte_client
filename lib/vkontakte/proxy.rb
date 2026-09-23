a2bf79c841917682eecee6918d040bbf2ca41e52
KlimSemikin <79899352+KlimSemikin@users.noreply.github.com>
2026-09-23 12:17:57 +0600

    feat: migrate VK API transport to Faraday Typhoeus (#1)
    * feat: migrate VK API transport to Faraday Typhoeus
    
    * remove ruby dependency
    
    * feat: support configurable Faraday adapters

--- lib/vkontakte/proxy.rb
+++ lib/vkontakte/proxy.rb
@@ -4,7 +4,7 @@ module Vkontakte
   class Proxy # :nodoc:
     attr_reader :type, :addr, :port, :user, :password
 
-    VALID_PROXY_TYPES = %i[http socks].freeze
+    VALID_PROXY_TYPES = %i[http https socks].freeze
 
     def initialize(type, addr, port, user = nil, password = nil)
       raise(StandartError, "#{`type`} is an invalid proxy type. Available values: #{VALID_PROXY_TYPES.join(',')}") unless VALID_PROXY_TYPES.include?(type)
--- vkontakte_client.gemspec
+++ vkontakte_client.gemspec
@@ -17,6 +17,7 @@ Gem::Specification.new do |gem|
   gem.require_paths = ['lib']
   gem.version       = Vkontakte::VERSION
   gem.required_ruby_version = '>= 3.2.0'
+  gem.add_runtime_dependency 'faraday', '~> 2.14'
   gem.add_runtime_dependency 'mechanize', '~> 2.8', '>= 2.8.0'
   gem.add_runtime_dependency 'socksify', '~> 1.7', '>= 1.7.0'
   gem.metadata['rubygems_mfa_required'] = 'true'
